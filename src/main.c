e9c985b34ace4c4f8e90181ece3bf6dcc8d3e282
xuweitong <xuweitonga@163.com>
2018-07-09 16:38:01 +0800

    1

---
+++ src/main.c
@@ -0,0 +1,125 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/types.h>
+
+#ifdef DEBUG
+#define LOG(format, ...) fprintf(stdout, ">>>>>" format "<<<<", ##__VA_ARGS__)
+#else
+#define LOG(format, ...)
+#endif
+
+pthread_mutex_t g_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t g_cond = PTHREAD_COND_INITIALIZER;
+
+void sighandler(int signo)
+{
+    int status;
+    pthread_cond_signal(g_cond);
+    LOG("recv signal %d", signo);
+}
+void childsighandler(int signo)
+{
+    pid_t pid;
+    int stat;
+    while (pid = waitpid(-1, &stat, WNOHANG) > 0)
+    {
+        LOG("child pid [%d] exit.", pid);
+    }
+}
+
+void procsig()
+{
+    struct sigaction act;
+    act.sa_handler = SIGIGN;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGINT, &act, NULL);
+    sigaction(SIGHUP, &act, NULL);
+    sigaction(SIGQUIT, &act, NULL);
+    sigaction(SIGCHLD, &act, NULL);
+    sigaction(SIGPIPE, &act, NULL);
+    sigaction(SIGTTIN, &act, NULL);
+    sigaction(SIGTTOU, &act, NULL);
+    sigaction(SIGTSTP, &act, NULL);
+
+    act.sa_handler = childsighandler;
+    sigaction(SIGCHLD, &act, NULL);
+
+    act.sa_handler = sighandler;
+    sigaction(SIGUSR1, &act, NULL);
+}
+
+void control(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        LOG("Usage: %s <cmd> \n", argv[0]);
+        LOG("    start:        start %s. \n", argv[0]);
+        LOG("    stop:         stop %s. \n", argv[0]);
+        exit(0);
+    }
+
+    if (strcmp(argv[1], "start") == 0)
+    {
+        lockfd = open("./lockfile.lock", O_RDWR | O_CREAT, 0640);
+        if (lockfd < 0)
+        {
+            LOG("start %s failure[open lock error]!!", argv[0]);
+            exit(-1);
+        }
+
+        iRetVal = lockf(lockfd, F_TLOCK, 0);
+        if (iRetVal < 0)
+        {
+            LOG("%s is already running!!", argv[0]);
+            close(lockfd);
+            exit(-1);
+        }
+
+        memset(szBuff, 0x00, sizeof(szBuff));
+        sprintf(szBuff, "%6d\n", getpid());
+        write(lockfd, szBuff, strlen(szBuff));
+    }
+    else if (strcmp(argv[1], "stop") == 0)
+    {
+        lockfd = open("./lockfile.lock", O_RDWR | O_CREAT, 0640);
+        if (lockfd < 0)
+        {
+            LOG("stop %s failure[open lock error]!!", argv[0]);
+            exit(-1);
+        }
+        memset(szPid, 0x00, sizeof(szPid));
+        iRetVal = read(lockfd, szPid, sizeof(szPid));
+        close(lockfd);
+        kill(atoi(szPid), SIGUSR1);
+        exit(0);
+    }
+    else
+    {
+        LOG("command error\n\n");
+        exit(-1);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    procsig();
+    control(argc,argv);
+
+    //TODO ... 功能处理
+
+
+    //阻塞主线程
+    pthread_mutex_lock(&g_mutex);
+    pthread_cond_wait(&g_cond,&g_mutex);
+    pthread_mutex_unlock(&g_mutex);
+
+    //TODO ... 资源回收
+
+    LOG("%s stopped...\n")
+
+    return 0;
+}
