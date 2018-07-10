2a5c976805def21f62295039600b464412f77ff3
Biu <biu@localhost.localdomain>
2018-07-10 10:00:23 +0800

    1

--- src/main.c
+++ src/main.c
@@ -122,10 +122,8 @@ int main(int argc, char **argv)
 
 
     //阻塞主线程
-    pthread_mutex_lock(&g_mutex);
-    sleep(2);
-    pthread_mutex_lock(&g_mutex);
     pthread_cond_wait(&g_cond,&g_mutex);
+    pthread_mutex_lock(&g_mutex);
     pthread_mutex_unlock(&g_mutex);
 
     //TODO ... 资源回收
